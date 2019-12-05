5bbc45294ce95f647b88999ac3efa24e6866e6a6
Innod <fomichev.roman@yandex.ru>
2019-12-05 20:40:47 +0300

    added rate-yo

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,17 @@
 $(function() {
 
+    $(".slider__stars").rateYo({
+        rating: 4.5,
+        readOnly: true
+      });
+
     $('.slider__inner').slick({
         arrows: true,
         prevArrow: '<button class="slick-arrow prev-arrow icon-angle-left"></button>',
         nextArrow: '<button class="slick-arrow next-arrow icon-angle-right"></button>'
     });
+
+    
 
 });
 
--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,8 @@ gulp.task('script',function(){
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/chosen-js/chosen.jquery.js',
         'node_modules/chosen-js/chosen.proto.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+        'node_modules/rateyo/src/jquery.rateyo.js'
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
@@ -35,7 +36,8 @@ gulp.task('style',function(){
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/chosen-js/chosen.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css',
+        'node_modules/rateyo/src/jquery.rateyo.css'
     ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
