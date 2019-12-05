1bf70ce6e20b92a1f252bb381290b7dc6a3cd656
Innod <fomichev.roman@yandex.ru>
2019-12-05 20:26:21 +0300

    added advantages and slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
+$(function() {
 
+    $('.slider__inner').slick({
+        arrows: true,
+        prevArrow: '<button class="slick-arrow prev-arrow icon-angle-left"></button>',
+        nextArrow: '<button class="slick-arrow next-arrow icon-angle-right"></button>'
+    });
+
+});
 
    
