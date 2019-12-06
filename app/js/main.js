2c491fd536be02435256653b4735fc838d1d8c7c
Innod <fomichev.roman@yandex.ru>
2019-12-06 14:50:15 +0300

    added slider-followers

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,14 @@ $(function() {
         nextArrow: '<button class="slick-arrow next-arrow icon-angle-right"></button>'
     });
 
+    $('.slider-followers__inner').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: '<button class="slick-arrow prev-arrow icon-angle-left"></button>',
+        nextArrow: '<button class="slick-arrow next-arrow icon-angle-right"></button>'
+      });
+
 
     var mixer = mixitup('.products__inner');
 
